8d9da0d907b684eb0372c98dc28f7cff7696bcbf
MatiasRosbaco <mati.rosbaco@gmail.com>
2022-11-09 13:38:40 -0300

    subiendo proyecto denuevo

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+const apiController = require('../controllers/controllerEnvio');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/enviarMails',apiController.sendMail);
+
+module.exports = router;
