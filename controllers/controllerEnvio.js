5acdc3f145f63f84cf58698b66be6902b228b86b
mayrerosbaco <mayre.romero@rosbacopartners.com>
2023-04-05 12:06:58 -0300

    Cambios en el texto de envio de cuotas

--- controllers/controllerEnvio.js
+++ controllers/controllerEnvio.js
@@ -1,78 +1,263 @@
-const sgMail = require('@sendgrid/mail');
-let fs = require('fs');
-let path = require('path');
-let creds = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../salesforce-creds.json')).toString());
-var jsforce1 = require('../jsforce.js');
-sgMail.setApiKey(process.env.SendGridKey);// cambiar a variable de ambiente
+const sgMail = require("@sendgrid/mail");
+let fs = require("fs");
+let path = require("path");
+let creds = JSON.parse(
+  fs
+    .readFileSync(path.resolve(__dirname, "../salesforce-creds.json"))
+    .toString()
+);
+var jsforce1 = require("../jsforce.js");
+sgMail.setApiKey(process.env.SendGridKey); // cambiar a variable de ambiente
 
+const controllers = {
+  sendMail: async (req, res) => {
+    let respuesta;
+    console.log(req.query);
+    let idProyecto = req.query.idProyecto;
+    let objetos = await jsforce1.connection(idProyecto);
+    let proyecto = objetos.proyectoObj.records[0];
+    console.log(proyecto);
+    console.log(objetos.estadosObj);
+    try {
+      const messages = objetos.estadosObj.records.map((estado) => {
+        let saldoPesos;
+        let importeCac;
+        let importeDiferencia;
+        let importeDolares;
+        let saldoDolares;
+        let textoPesos;
+        let textoDolares;
 
-const controllers={
-    sendMail:async(req,res) => {
-        let respuesta;
-        console.log(req.query);
-        let idProyecto = req.query.idProyecto;
-        let objetos = await jsforce1.connection(idProyecto);
-        let proyecto = objetos.proyectoObj.records[0];
-        console.log(proyecto);
-        console.log(objetos.estadosObj);
-        try{
-            const messages = objetos.estadosObj.records.map((estado)=>{
-                let saldoPesos;
-                let importeCac;
-                let importeDiferencia;
-                let importeDolares;
-                let saldoDolares;
-                console.log(proyecto.Numero_de_cuenta_dolares__c);
-                if(estado.Saldo_a_cobrar_en_pesos__c == undefined){
-                    saldoPesos = 0;
-                    importeCac = 0;
-                    importeDiferencia = 0;
-                }else{
-                    saldoPesos = estado.Saldo_a_cobrar_en_pesos__c
-                    importeCac = estado.Importe_mas_cac_aid__c;
-                    importeDiferencia = estado.Importe_diferencia_de_indice__c;
-                }
-                if(estado.Saldo_a_cobrar_en_dolares__c == undefined){
-                    saldoDolares = 0;
-                    importeDolares = 0;
-                }else{
-                    saldoDolares = estado.Saldo_a_cobrar_en_dolares__c;
-                    importeDolares = estado.Importe_en_dolares__c;
-                }
-                return{
-                    to: [estado.Email_propietario_1__c,estado.Email_propietario_2__c,estado.Email_propietario_3__c,"comunidad.rosbaco@gmail.com"],
-                    from: 'clientes@rosbacopartners.com',
-                    subject: 'Informacion de saldos '+estado.Nombre_unidad__c+'',
-                    text: 'Estimado/a '+estado.Nombre_propietario__c+',Le informamos que el saldo a abonar por precio (*) hasta el día '+estado.Fecha__c+', correspondiente al boleto de compraventa de referencia, asciende a la suma de PESOS '+saldoPesos+' detallado de la siguiente manera en la cuota actual, importe con cac: '+importeCac+' + importe diferencia por indice: '+importeDiferencia+' y suma en DOLARES '+saldoDolares+' con el detalle de la cuota actual de:'+importeDolares+' En cumplimiento con las disposiciones legales vigentes, todos los pagos en pesos correspondientes al pago del precio de tu boleto de compraventa, corresponderá realizarse vía transferencia o vía depósito bancario a la cuenta debajo detallada, sin excepción, con posterior envío de comprobante de pago por este medio  Datos de la cuenta bancaria: - Razón social:'+proyecto.Razon_social__c+'   - CUIT: '+proyecto.Cuit__c+'  - Banco:'+proyecto.Banco__c+'   Cuenta en pesos: - CBU: '+proyecto.Cbu_pesos__c+' - Alias:'+proyecto.Alias_pesos__c+' - Número de cuenta:'+proyecto.Numero_de_cuenta_pesos__c+' Cuenta en dolares: - CBU: '+proyecto.Cbu_dolares__c+' - Alias:'+proyecto.Alias_dolares__c+' - Número de cuenta:'+proyecto.Numero_de_cuenta_dolares__c+' Nos mantenemos a disposición en caso de dudas o consultas por los medios habituales.  Notas:  (*) El saldo informado no incluye intereses punitorios por deudas anteriores. En caso de corresponder aplicación de intereses, estos serán informados una vez cancelada la deuda. Como siempre, a disposición. Saludamos atentamente  Equipo de Comunidad Rosbaco & Partners Contacto: (011) 15-5613-1675 ',
-                    html: '<p>Estimado/a '+estado.Nombre_propietario__c+'<br>,Le informamos que el saldo a abonar por precio (*) hasta el día '+estado.Fecha__c+', correspondiente al boleto de compraventa de referencia, asciende a la suma de PESOS '+saldoPesos+' detallado de la siguiente manera en la cuota actual, importe con cac: '+importeCac+' + importe diferencia por indice: '+importeDiferencia+' y suma en DOLARES '+saldoDolares+' con el detalle de la cuota actual de:'+importeDolares+' <br> En cumplimiento con las disposiciones legales vigentes, todos los pagos en pesos correspondientes al pago del precio de tu boleto de compraventa, corresponderá realizarse vía transferencia o vía depósito bancario a la cuenta debajo detallada, sin excepción, con posterior envío de comprobante de pago por este medio:<br>  Datos de la cuenta bancaria: <br> -Razón social:  '+proyecto.Razon_social__c+'   <br>-CUIT: '+proyecto.Cuit__c+'  <br> -Banco: '+proyecto.Banco__c+'  <br> Cuenta en pesos: <br> -CBU: '+proyecto.Cbu_pesos__c+' <br> -Alias: '+proyecto.Alias_pesos__c+' <br> -Número de cuenta: '+proyecto.Numero_de_cuenta_pesos__c+' <br> Cuenta en dolares: <br> - CBU: '+proyecto.Cbu_dolares__c+' <br> - Alias:'+proyecto.Alias_dolares__c+' <br> - Número de cuenta:'+proyecto.Numero_de_cuenta_dolares__c+'<br> Nos mantenemos a disposición en caso de dudas o consultas por los medios habituales.  <br>Notas:  (*) El saldo informado no incluye intereses punitorios por deudas anteriores. En caso de corresponder aplicación de intereses, estos serán informados una vez cancelada la deuda. <br>Como siempre, a disposición. <br> Saludamos atentamente  <br> Equipo de Comunidad <br>Rosbaco & Partners <br>Contacto: <br>(011) 15-5613-1675 </p>', 
-                }
-            })
-        
-        await sgMail.send(messages).then(()=>{
-            console.log('Emails enviados');
-            respuesta = {
-                meta: {
-                    status: 200,
-                    data:{
-                        estadosID: objetos.estadosObj.records.map((estado)=>{
-                            return estado.nombre_unidad__c;
-                        })},
-                    }
-                }
-        }).catch(error =>{
-            console.log(error);
+        console.log(proyecto.Numero_de_cuenta_dolares__c);
+        if (estado.Tipo_de_plan__c == "Pesos") {
+          if (estado.Saldo_a_cobrar_en_pesos__c == undefined) {
+            saldoPesos = 0;
+            importeCac = 0;
+            importeDiferencia = 0;
+          } else {
+            saldoPesos = estado.Saldo_a_cobrar_en_pesos__c;
+            importeCac = estado.Importe_mas_cac_aid__c;
+            importeDiferencia = estado.Importe_diferencia_de_indice__c;
+          }
+          return textoPesos;
+        }
+        if (estado.Tipo_de_plan__c == "Dolar") {
+          if (estado.Saldo_a_cobrar_en_dolares__c == undefined) {
+            saldoDolares = 0;
+            importeDolares = 0;
+          } else {
+            saldoDolares = estado.Saldo_a_cobrar_en_dolares__c;
+            importeDolares = estado.Importe_en_dolares__c;
+          }
+
+          return textoDolares;
+        }
+
+        textoPesos = {
+          to: [
+            estado.Email_propietario_1__c,
+            estado.Email_propietario_2__c,
+            estado.Email_propietario_3__c,
+            "comunidad.rosbaco@gmail.com",
+          ],
+          from: "clientes@rosbacopartners.com",
+          subject: "Informacion de saldos " + estado.Nombre_unidad__c + "",
+          text:
+            "Estimado/a " +
+            estado.Nombre_propietario__c +
+            "Le informamos que al dia de la fecha" +
+            estado.Fecha__c +
+            "el monto a abonar por la compra de la Unidad de referencia esta compuesto por:" +
+            "1) La suma de $ " +
+            saldoPesos +
+            " correspondiente a la " +
+            Plan_de_pago_en_pesos__c +
+            "de su plan de pagos;" +
+            " La cuenta habilitada a fin de realizar correctamente su/s pago/s es la siguiente: " +
+            "- Razón social:" +
+            proyecto.Razon_social__c +
+            "- Cuit: " +
+            proyecto.Cuit__c +
+            "- Banco:" +
+            proyecto.Banco__c +
+            "- Cuenta en pesos: - CBU: " +
+            proyecto.Cbu_pesos__c +
+            "- Alias:" +
+            proyecto.Alias_pesos__c +
+            "- Número de cuenta:" +
+            proyecto.Numero_de_cuenta_pesos__c +
+            "Recuerde que, atento a las disposiciones vigentes (art.1° Ley 25.345 y cctes.), todos los pagos superiores a $ 1.000 deberán ser realizados mediante transferencia y/o depósito bancario.Una vez que haya podido realizar el pago de la/s suma/s indicada/s, le solicitamos tenga a bien responder a este mismo correo, adjuntando el/los correspondiente/s comprobante/s para su correcta imputación." +
+            "Nota:" +
+            "(*) Para consultas dirigirse al correo nathalie.lossada@rosbacopartners.com.  " +
+            "(**) La cuota en pesos se encuentra calculada en base al índice CAC provisorio a la fecha " +
+            estado.Fecha__c +
+            ", con más los ajustes por corrección de índice de los meses anteriores. A los fines de su cálculo se ha realizado la siguiente operación:" +
+            "C = CB * (IB / IP) + CID" +
+            "Cuyas referencias son:" +
+            "C: Cuota" +
+            "CB: Cuota Base, pactada en su Boleto de Compraventa." +
+            "IB: Índice Base, según Boleto de Compraventa (conocido a la fecha de firma del Boleto)." +
+            "IP: Índice provisorio conocido a la fecha." +
+            "CID: Corrección por índice definitivo." +
+            "Se debe tener en cuenta que a la fecha LA PARTE VENDEDORA no cuenta con el índice CAC oficial definitivo del mes correspondiente al vencimiento de su cuota, sino con el determinado dos meses antes. Por tal motivo se utiliza un índice provisorio para el cálculo de la cuota del mes y se le adiciona el ajuste por corrección de índice definitivo de las cuotas anteriores (las cuales también fueron liquidadas con índices provisorios)" +
+            "Quedamos a disposición por cualquier inquietud." +
+            "Atentos saludos," +
+            "Equipo de Comunidad" +
+            "Rosbaco & Partners" +
+            "Contacto: (011) 15-5613-1675",
+          html:
+            "<p>Estimado/a " +
+            estado.Nombre_propietario__c +
+            "," +
+            "<br>Le informamos que al dia de la fecha" +
+            estado.Fecha__c +
+            "," +
+            "el monto a abonar por la compra de la Unidad de referencia esta compuesto por: " +
+            "<br><br>1) La suma de $ " +
+            saldoPesos +
+            " correspondiente a la " +
+            Plan_de_pago_en_pesos__c +
+            " de su plan de pagos;" +
+            "<br><br>La cuenta habilitada a fin de realizar correctamente su/s pago/s es la siguiente: " +
+            "<br>- Razón social:" +
+            proyecto.Razon_social__c +
+            "<br>- Cuit: " +
+            proyecto.Cuit__c +
+            "<br>- Banco:" +
+            proyecto.Banco__c +
+            "<br>- Cuenta en pesos: - CBU: " +
+            proyecto.Cbu_pesos__c +
+            "<br>- Alias:" +
+            proyecto.Alias_pesos__c +
+            "<br>- Número de cuenta:" +
+            proyecto.Numero_de_cuenta_pesos__c +
+            "<br><br>Recuerde que, atento a las disposiciones vigentes (art.1° Ley 25.345 y cctes.), todos los pagos superiores a $ 1.000 deberán ser realizados mediante transferencia y/o depósito bancario.Una vez que haya podido realizar el pago de la/s suma/s indicada/s, le solicitamos tenga a bien responder a este mismo correo, adjuntando el/los correspondiente/s comprobante/s para su correcta imputación." +
+            "<br><br>Nota:" +
+            "<br><br>(*) Para consultas dirigirse al correo nathalie.lossada@rosbacopartners.com.  " +
+            "<br><br>(**) La cuota en pesos se encuentra calculada en base al índice CAC provisorio a la fecha " +
+            estado.Fecha__c +
+            ", con más los ajustes por corrección de índice de los meses anteriores. A los fines de su cálculo se ha realizado la siguiente operación:" +
+            "<br><br>C = CB * (IB / IP) + CID" +
+            "<br>Cuyas referencias son:" +
+            "<br><br>C: Cuota" +
+            "<br>CB: Cuota Base, pactada en su Boleto de Compraventa." +
+            "<br>IB: Índice Base, según Boleto de Compraventa (conocido a la fecha de firma del Boleto)." +
+            "<br>IP: Índice provisorio conocido a la fecha." +
+            "<br>CID: Corrección por índice definitivo." +
+            "<br><br>Se debe tener en cuenta que a la fecha LA PARTE VENDEDORA no cuenta con el índice CAC oficial definitivo del mes correspondiente al vencimiento de su cuota, sino con el determinado dos meses antes. Por tal motivo se utiliza un índice provisorio para el cálculo de la cuota del mes y se le adiciona el ajuste por corrección de índice definitivo de las cuotas anteriores (las cuales también fueron liquidadas con índices provisorios)" +
+            "<br><br>Quedamos a disposición por cualquier inquietud." +
+            "<br><br>Atentos saludos," +
+            "<br>Equipo de Comunidad" +
+            "<br>Rosbaco & Partners" +
+            "<br>Contacto: (011) 15-5613-1675</p>",
+        };
+        textoDolares = {
+          to: [
+            estado.Email_propietario_1__c,
+            estado.Email_propietario_2__c,
+            estado.Email_propietario_3__c,
+          ],
+          from: "clientes@rosbacopartners.com",
+          subject: "Informacion de saldos " + estado.nombre_unidad__c + "",
+          text:
+            "Estimado/a " +
+            estado.nombre_propietario__c +
+            "Le informamos que al dia de la fecha " +
+            estado.fecha__c +
+            "el monto a abonar por la compra de la Unidad de referencia esta compuesta por: " +
+            "La Suma de USD " +
+            saldoDolares +
+            "correspondiente a la " +
+            Plan_de_pago_en_dolares__c +
+            " de su plan de pagos;" +
+            "La cuenta habilitada a fin de realizar correctamente su/s pago/s es la siguiente:" +
+            "Datos de la cuenta bancaria:" +
+            "- Razón social:" +
+            proyecto.razon_social__c +
+            "- Cuit: " +
+            proyecto.cuit__c +
+            "- Banco:" +
+            proyecto.banco__c +
+            "- Cuenta en dolares: - CBU: " +
+            proyecto.cbu_dolares__c +
+            "- Alias:" +
+            proyecto.alias_dolares__c +
+            "- Número de cuenta:" +
+            proyecto.numero_de_cuenta_dolares__c +
+            "Recuerde que, atento a las disposiciones vigentes (art.1° Ley 25.345 y cctes.), todos los pagos superiores a $ 1.000 deberán ser realizados mediante transferencia y/o depósito bancario.Una vez que haya podido realizar el pago de la/s suma/s indicada/s, le solicitamos tenga a bien responder a este mismo correo, adjuntando el/los correspondiente/s comprobante/s para su correcta imputación." +
+            "Nota:" +
+            "(*) Para consultas dirigirse al correo nathalie.lossada@rosbacopartners.com.  " +
+            "Quedamos a disposición por cualquier inquietud." +
+            "Atentos saludos," +
+            "Equipo de Comunidad" +
+            "Rosbaco & Partners" +
+            "Contacto: (011) 15-5613-1675",
+          html:
+            "<p>Estimado/a " +
+            estado.nombre_propietario__c +
+            "<br>Le informamos que al dia de la fecha " +
+            estado.fecha__c +
+            "el monto a abonar por la compra de la Unidad de referencia esta compuesta por: " +
+            "<br><br>La Suma de USD " +
+            saldoDolares +
+            "correspondiente a la " +
+            Plan_de_pago_en_dolares__c +
+            " de su plan de pagos;" +
+            "<br>La cuenta habilitada a fin de realizar correctamente su/s pago/s es la siguiente:" +
+            "<br><br>Datos de la cuenta bancaria:" +
+            "<br>- Razón social:" +
+            proyecto.razon_social__c +
+            "<br>- Cuit: " +
+            proyecto.cuit__c +
+            "<br>- Banco:" +
+            proyecto.banco__c +
+            "<br>- Cuenta en dolares:" +
+            "<br><br>- CBU: " +
+            proyecto.cbu_dolares__c +
+            "<br>- Alias:" +
+            proyecto.alias_dolares__c +
+            "<br>- Número de cuenta:" +
+            proyecto.numero_de_cuenta_dolares__c +
+            "<br><br>Recuerde que, atento a las disposiciones vigentes (art.1° Ley 25.345 y cctes.), todos los pagos superiores a $ 1.000 deberán ser realizados mediante transferencia y/o depósito bancario.Una vez que haya podido realizar el pago de la/s suma/s indicada/s, le solicitamos tenga a bien responder a este mismo correo, adjuntando el/los correspondiente/s comprobante/s para su correcta imputación." +
+            "<br><br>Nota:" +
+            "<br>(*) Para consultas dirigirse al correo nathalie.lossada@rosbacopartners.com.  " +
+            "<br>Quedamos a disposición por cualquier inquietud." +
+            "<br><br>Atentos saludos," +
+            "<br>Equipo de Comunidad" +
+            "<br>Rosbaco & Partners" +
+            "<br>Contacto: (011) 15-5613-1675 </p>",
+        };
+      });
+
+      await sgMail
+        .send(messages)
+        .then(() => {
+          console.log("Emails enviados");
+          respuesta = {
+            meta: {
+              status: 200,
+              data: {
+                estadosID: objetos.estadosObj.records.map((estado) => {
+                  return estado.nombre_unidad__c;
+                }),
+              },
+            },
+          };
         })
-    }catch(error){
-        console.error(error)
-                respuesta = {
-                    meta: {
-                        status: 400,
-                    }
-                }
-        throw error;
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.error(error);
+      respuesta = {
+        meta: {
+          status: 400,
+        },
+      };
+      throw error;
     }
     res.json(respuesta);
-    }
-}
+  },
+};
 
 module.exports = controllers;
--- jsforce.js
+++ jsforce.js
@@ -1,44 +1,55 @@
-var jsforce = require('jsforce');
-var fs = require('fs');
-var path = require('path');
+var jsforce = require("jsforce");
+var fs = require("fs");
+var path = require("path");
 const controller = {
-    connection: async function(idProyecto){
-        const today = new Date();
-        let month = today.getMonth() + 1;
-            var conn = new jsforce.Connection({
-                loginUrl: process.env.url
-            });
-            try {
-                await conn.login(process.env.usernameSF, process.env.password+process.env.securityToken );//cambiar a ouath y variable de ambiente
-                console.log('Connected to Salesforce!');
-                //creds.accessToken = conn.accessToken;
-                //creds.instanceUrl = conn.instanceUrl;
-                //console.log(creds.accessToken);
-                //console.log(creds.instanceUrl);
-                // now you can use conn to read/write data...
-                let soqlProyecto = "SELECT 	Banco__c,Razon_social__c,Numero_de_cuenta_pesos__c,alias_pesos__c,cbu_pesos__c, Cuit__c, numero_de_cuenta_dolares__c,cbu_dolares__c,alias_dolares__c FROM obra__c WHERE id = '"+ idProyecto + "'LIMIT 1";
-                var proyecto = await conn.query(soqlProyecto);
-                let soqlUnidad = "SELECT id FROM unidad__c WHERE obra__c = '"+ idProyecto +"'";
-                let unidades = await conn.query(soqlUnidad);
-                let idUnidades = unidades.records.map((unidad => {
-                    return unidad.Id;
-                }));
-                let soqlEstados = "SELECT 	Importe_mas_cac_aid__c,Importe_en_dolares__c,Importe_diferencia_de_indice__c,fecha__c,Nombre_propietario__c,Nombre_unidad__c,Email_propietario_1__c,Email_propietario_2__c,Email_propietario_3__c,Saldo_a_cobrar_en_dolares__c,Saldo_a_cobrar_en_pesos__c FROM estado_de_cuentas__c WHERE unidad__c IN ('" + idUnidades.join("','") + "') " + "AND mes__c = "+month+" AND Email_enviado__c = false LIMIT 200";
-                var estados = await conn.query(soqlEstados);
-                console.log('los estados son' + estados);
-                await conn.logout();
-                console.log('connection logged out');
-            } catch (err) { 
-                console.error(err);
-            }
-            return objetos = {
-                estadosObj:estados,
-                proyectoObj:proyecto
-            };
-    },
-    getEstados: async()=>{
-
+  connection: async function (idProyecto) {
+    const today = new Date();
+    let month = today.getMonth() + 1;
+    var conn = new jsforce.Connection({
+      loginUrl: process.env.url,
+    });
+    try {
+      await conn.login(
+        process.env.usernameSF,
+        process.env.password + process.env.securityToken
+      ); //cambiar a ouath y variable de ambiente
+      console.log("Connected to Salesforce!");
+      //creds.accessToken = conn.accessToken;
+      //creds.instanceUrl = conn.instanceUrl;
+      //console.log(creds.accessToken);
+      //console.log(creds.instanceUrl);
+      // now you can use conn to read/write data...
+      let soqlProyecto =
+        "SELECT 	Banco__c,Razon_social__c,Numero_de_cuenta_pesos__c,alias_pesos__c,cbu_pesos__c, Cuit__c, numero_de_cuenta_dolares__c,cbu_dolares__c,alias_dolares__c FROM obra__c WHERE id = '" +
+        idProyecto +
+        "'LIMIT 1";
+      var proyecto = await conn.query(soqlProyecto);
+      let soqlUnidad =
+        "SELECT id FROM unidad__c WHERE obra__c = '" + idProyecto + "'";
+      let unidades = await conn.query(soqlUnidad);
+      let idUnidades = unidades.records.map((unidad) => {
+        return unidad.Id;
+      });
+      let soqlEstados =
+        "SELECT 	Importe_mas_cac_aid__c,Importe_en_dolares__c,Importe_diferencia_de_indice__c,fecha__c,Nombre_propietario__c,Nombre_unidad__c,Email_propietario_1__c,Email_propietario_2__c,Email_propietario_3__c,Saldo_a_cobrar_en_dolares__c,Saldo_a_cobrar_en_pesos__c, Tipo_de_plan__c, Plan_de_pago_en_pesos__c, Plan_de_pago_en_dolares__c FROM estado_de_cuentas__c WHERE unidad__c IN ('" +
+        idUnidades.join("','") +
+        "') " +
+        "AND mes__c = " +
+        month +
+        " AND Email_enviado__c = false LIMIT 200";
+      var estados = await conn.query(soqlEstados);
+      console.log("los estados son" + estados);
+      await conn.logout();
+      console.log("connection logged out");
+    } catch (err) {
+      console.error(err);
     }
-}
+    return (objetos = {
+      estadosObj: estados,
+      proyectoObj: proyecto,
+    });
+  },
+  getEstados: async () => {},
+};
 
 module.exports = controller;
